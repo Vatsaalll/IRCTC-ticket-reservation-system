351e9bfae96c2961f2db35662235ca9b20ed1146
Vatsaalll <vatsalunadkat07@gmail.com>
2024-09-26 22:30:40 +0530

    change 3

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <string>
+
 class Coach {
 public:
     char type; 
@@ -24,5 +30,36 @@ public:
     
     void updateAvailableSeats(int numSeats) {
         availableSeats -= numSeats;
+    }
+};
+
+class Train {
+public:
+    std::string number; // Train number
+    std::pair<std::string, std::string> route; // Train route (from, to)
+    int distance; // Distance of the train route
+    std::vector<Coach> coaches; // Coaches in the train
+
+    Train(std::string number, std::pair<std::string, std::string> route, int distance, std::vector<Coach> coaches)
+        : number(number), route(route), distance(distance), coaches(coaches) {}
+
+    // Get the train number
+    std::string getNumber() const {
+        return number;
+    }
+
+    // Get the train route
+    std::pair<std::string, std::string> getRoute() const {
+        return route;
+    }
+
+    // Get the distance of the train route
+    int getDistance() const {
+        return distance;
+    }
+
+    // Get the coaches in the train
+    std::vector<Coach> getCoaches() const {
+        return coaches;
     }
 };
